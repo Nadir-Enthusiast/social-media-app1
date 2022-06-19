db61a9c490c1ec17728ea6cfb2c2716448677901
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2022-06-18 23:52:56 -0400

    Create SideProfile.js

---
+++ day1/components/sideProfile/SideProfile.js
@@ -0,0 +1,12 @@
+import "./SideProfile.css"
+import React from 'react'
+
+function SideProfile() {
+  return (
+    <div className="sideProfile">
+        <h1>Building Profile Settings</h1>
+    </div>
+  )
+}
+
+export default SideProfile
