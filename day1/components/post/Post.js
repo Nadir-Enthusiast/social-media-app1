f30982851a04b725afe97898c19f321c373943fc
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2022-06-18 23:51:16 -0400

    Create Post.js

---
+++ day1/components/post/Post.js
@@ -0,0 +1,28 @@
+import "./Post.css"
+import React from 'react'
+import Avatar from "@material-ui/core/Avatar";
+
+function Post({username, caption, profilePic, image}) {
+  return (
+    <div className="post">
+        <div className="post-header">
+            <Avatar
+                className="post-avatar" 
+                alt="ok"
+                src={profilePic}
+            />
+            <h2>{username}</h2>
+        </div>
+        <div className="post-img-container">
+            <img
+                className="post-img"
+                src={image}
+                alt="no"
+            />
+        </div>
+        <h4 className="post-text"><strong>{username}</strong> {caption}</h4>
+    </div>
+  )
+}
+
+export default Post
