f4ca20fe9fa0cb98c16d3f0f88750ce5d02201e7
Nadir <nadirkarimov76@gmail.com>
2022-07-23 12:03:58 -0400

    Security

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 import 'firebase/firestore'
 
 const firebaseApp = firebase.initializeApp({
-  //config
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: "social-media-app-by-nadir.firebaseapp.com",
+  projectId: "social-media-app-by-nadir",
+  storageBucket: "social-media-app-by-nadir.appspot.com",
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
 });
 
 const db = firebase.firestore();
