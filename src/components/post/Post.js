8d43243b31f00a21243f712983723e9b654170bb
Nadir <nadirkarimov76@gmail.com>
2022-08-03 23:41:53 -0400

    Update 7

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -6,8 +6,6 @@ import { Link } from "react-router-dom";
 
 function Post({postId, user, username, caption, profilePic, image}) {
 
-  console.log(postId, user, username, caption, profilePic, image)
-
   const [comments, setComments] = useState([]);
   const [comment, setComment] = useState('');
 
